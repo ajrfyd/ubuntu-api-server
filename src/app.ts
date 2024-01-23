306754db9769296353d7596c37da8c06e39af205
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-01-23 15:02:32 +0900

    Update: add cors uri

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.static(staticPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173", "https://k-log3943.netlify.app"],
+  origin: ["http://localhost:5173", "https://k-log3943.netlify.app", "http://localhost:5500"],
   methods: ["GET", "POST", "OPTIONS"],
   credentials: true
 }));
